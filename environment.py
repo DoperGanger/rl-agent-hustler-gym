bd3b9c2035188e643d6c0167689e115ec1d078d6
0xhatsume <0xhatsume@gmail.com>
2023-12-30 12:44:30 +0800

    :pencil2: Update indentation for env

--- environment.py
+++ environment.py
@@ -3,9 +3,10 @@ import numpy as np
 import random
 
 class Env():
-    def __init__(self, display, matrix, cat_mode, map_mode):
-        self.HEIGHT = matrix.ROWS # y
-        self.WIDTH = matrix.COLUMNS # x
+    
+    def __init__(self, display, rows, columns):
+        self.HEIGHT = rows # y
+        self.WIDTH = columns # x
         # self.CAT_MODE = cat_mode
         # self.MAP_MODE = map_mode
         self.OBSTACLES = []
@@ -25,125 +26,124 @@ class Env():
         # GOAL
         self.GOAL = pygame.transform.scale(pygame.image.load('sprites/door.png'),(self.BLOCK_WIDTH, self.BLOCK_HEIGHT))
 
-
-        # Set Obstacles Postion Array
-        def set_obstacles(self, obstacles:np.array): # Used to change the obstacles in the map
+    # Set Obstacles Postion Array ( array of indices)
+    def set_obstacles(self, obstacles:np.array): # Used to change the obstacles in the map
             self.OBSTACLES = obstacles
 
-        def render(self, i_episode = -1):
-            # Draw Agents
-            self.HUSTLER.draw(self.HUSTLER_X, self.HUSTLER_Y)
-            self.CATCHER.draw(self.CATCHER_X, self.CATCHER_Y)
-            self.DISPLAY.blit(self.GOAL, (self.GOAL_X*self.BLOCK_WIDTH, self.GOAL_Y*self.BLOCK_HEIGHT))
+    def render(self, i_episode = -1):
+        # Draw Agents
+        self.HUSTLER.draw(self.HUSTLER_X, self.HUSTLER_Y)
+        self.CATCHER.draw(self.CATCHER_X, self.CATCHER_Y)
+        self.DISPLAY.blit(self.GOAL, (self.GOAL_X*self.BLOCK_WIDTH, self.GOAL_Y*self.BLOCK_HEIGHT))
 
-            # Draw Obstacles
-            for pos in self.OBSTACLES:
-                pygame.draw.rect(self.DISPLAY, (0,0,255), [pos[0]*self.BLOCK_WIDTH, pos[1]*self.BLOCK_HEIGHT, self.BLOCK_WIDTH, self.BLOCK_HEIGHT])
+        # Draw Obstacles
+        for pos in self.OBSTACLES:
+            pygame.draw.rect(self.DISPLAY, (0,0,255), [pos[0]*self.BLOCK_WIDTH, pos[1]*self.BLOCK_HEIGHT, self.BLOCK_WIDTH, self.BLOCK_HEIGHT])
 
-            # Draw Info Panel
-            if i_episode>=0:
-                self.display_episode(i_episode)
+        # Draw Info Panel
+        if i_episode>=0:
+            self.display_episode(i_episode)
         
-        def reset(self):
-            '''
-            Used to reset all elements position in the environment
-            '''
-
-            # get set of map indices in a flat array
-            indices = np.arange(self.WIDTH * self.HEIGHT)
-
-            # remove indices of obstacles
-            for v in indices:
-                if v in self.OBSTACLES:
-                    # pop value out of indices
-                    indices = np.delete(indices, np.where(indices == v))
-
-            # Random set all agent positions
-            self.HUSTLER_X, self.HUSTLER_Y = self.indexToXY(self.getRandomIndexPos(indices))
-            self.CATCHER_X, self.CATCHER_Y = self.indexToXY(self.getRandomIndexPos(indices))
-            self.GOAL_X, self.GOAL_Y = self.indexToXY(self.getRandomIndexPos(indices))
-
-            self.MOVES['hustler'] = 100
-            self.MOVES['catcher'] = 100
-
-            return self.get_state()
-
-        def get_state(self):
-            '''
-            Return the state for the agent:
-                Hustler:
-                    - Manhattan distance between hustler and catcher
-                    - Manhattan distance between hustler and goal
-                    - Info about walls and obstacles in their neighborood?
-                Catcher:
-                    - Manhattan distance between hustler and catcher
-                    - Info about walls and obstacles in their neighborood?
-            '''
-            self.STATE = {'hustler':(self.HUSTLER_X - self.CATCHER_X, self.HUSTLER_Y - self.CATCHER_Y,
-                                    self.HUSTLER_X - self.GOAL_X, self.HUSTLER_Y -  self.GOAL_Y), 
-                        'catcher':(self.CATCHER_X - self.HUSTLER_X, self.CATCHER_Y - self.HUSTLER_Y)}
-
-            return self.STATE
+    def reset(self):
+        '''
+        Used to reset all elements position in the environment
+        '''
         
-        def step(self, hustler_action, catcher_action):
-            '''
-            Reward update, change angents' position and do some controls about position changes.
-            '''
-            reward = {'hustler':-1, 'catcher':-1}
-            done = False
-            info = {
-                'goal_reached': False,
-                'hustler_caught': False, 
-                'x': -1, 'y': -1, 
-                'width':self.BLOCK_WIDTH, 
-                'height':self.BLOCK_HEIGHT
-            }
+        # get set of map indices in a flat array
+        indices = np.arange(self.WIDTH * self.HEIGHT)
 
-            #decreasing the no. of moves
-            self.MOVES['catcher'] -= 1
-            self.MOVES['hustler'] -= 1
-            #done if moves = 0
-            if self.MOVES['catcher'] == 0 or self.MOVES['hustler'] == 0:
-                done = True
+        # remove indices of obstacles
+        for v in indices:
+            if v in self.OBSTACLES:
+                # pop value out of indices
+                indices = np.delete(indices, np.where(indices == v))
 
-            self.update_positions(hustler_action, catcher_action)
-            
-            #hustler reached the goal
-            if self.HUSTLER_X == self.GOAL_X and self.HUSTLER_Y == self.GOAL_Y:
-                done = True
-                reward['hustler'] = 50
-                info['goal_reached'], info['x'], info['y'] = True,  self.HUSTLER_X, self.HUSTLER_Y
-            
-            #catcher caught the hustler
-            if self.CATCHER_X == self.HUSTLER_X and self.CAT_Y == self.HUSTLER_Y:
-                done = True
-                reward['catcher'] = 50
+        # Random set all agent positions
+        self.HUSTLER_X, self.HUSTLER_Y = self.indexToXY(self.getRandomIndexPos(indices))
+        self.CATCHER_X, self.CATCHER_Y = self.indexToXY(self.getRandomIndexPos(indices))
+        self.GOAL_X, self.GOAL_Y = self.indexToXY(self.getRandomIndexPos(indices))
+
+        self.MOVES['hustler'] = 100
+        self.MOVES['catcher'] = 100
+
+        return self.get_state()
+
+    def get_state(self):
+        '''
+        Return the state for the agent:
+            Hustler:
+                - Manhattan distance between hustler and catcher
+                - Manhattan distance between hustler and goal
+                - Info about walls and obstacles in their neighborood?
+            Catcher:
+                - Manhattan distance between hustler and catcher
+                - Info about walls and obstacles in their neighborood?
+        '''
+        self.STATE = {'hustler':(self.HUSTLER_X - self.CATCHER_X, self.HUSTLER_Y - self.CATCHER_Y,
+                                self.HUSTLER_X - self.GOAL_X, self.HUSTLER_Y -  self.GOAL_Y), 
+                    'catcher':(self.CATCHER_X - self.HUSTLER_X, self.CATCHER_Y - self.HUSTLER_Y)}
+
+        return self.STATE
+        
+    def step(self, hustler_action, catcher_action):
+        '''
+        Reward update, change angents' position and do some controls about position changes.
+        '''
+        reward = {'hustler':-1, 'catcher':-1}
+        done = False
+        info = {
+            'goal_reached': False,
+            'hustler_caught': False, 
+            'x': -1, 'y': -1, 
+            'width':self.BLOCK_WIDTH, 
+            'height':self.BLOCK_HEIGHT
+        }
+
+        #decreasing the no. of moves
+        self.MOVES['catcher'] -= 1
+        self.MOVES['hustler'] -= 1
+        #done if moves = 0
+        if self.MOVES['catcher'] == 0 or self.MOVES['hustler'] == 0:
+            done = True
+
+        self.update_positions(hustler_action, catcher_action)
+        
+        #hustler reached the goal
+        if self.HUSTLER_X == self.GOAL_X and self.HUSTLER_Y == self.GOAL_Y:
+            done = True
+            reward['hustler'] = 50
+            info['goal_reached'], info['x'], info['y'] = True,  self.HUSTLER_X, self.HUSTLER_Y
+        
+        #catcher caught the hustler
+        if self.CATCHER_X == self.HUSTLER_X and self.CAT_Y == self.HUSTLER_Y:
+            done = True
+            reward['catcher'] = 50
+            reward['hustler'] = -20
+            info['hustler_caught'], info['x'], info['y'] = True,  self.HUSTLER_X, self.HUSTLER_Y
+        
+        for obs in self.OBSTACLES:
+            if self.HUSTLER_X == obs[0] and self.HUSTLER_Y == obs[1]:
                 reward['hustler'] = -20
-                info['hustler_caught'], info['x'], info['y'] = True,  self.HUSTLER_X, self.HUSTLER_Y
-            
-            for obs in self.OBSTACLES:
-                if self.HUSTLER_X == obs[0] and self.HUSTLER_Y == obs[1]:
-                    reward['hustler'] = -20
-                    self.HUSTLER_X, self.HUSTLER_Y = (0,0)
+                self.HUSTLER_X, self.HUSTLER_Y = (0,0)
 
-                if self.CATCHER_X == obs[0] and self.CATCHER_Y == obs[1]:    
-                    reward['catcher'] = -20
-                    self.CATCHER_X, self.CATCHER_Y = (0, self.HEIGHT -1)
+            if self.CATCHER_X == obs[0] and self.CATCHER_Y == obs[1]:    
+                reward['catcher'] = -20
+                self.CATCHER_X, self.CATCHER_Y = (0, self.HEIGHT -1)
 
-            return self.get_state(), reward, done, info
+        return self.get_state(), reward, done, info
         
-        def getRandomIndexPos(self, indices):
-            return indices[np.random.randint(0, len(indices))]
+    def getRandomIndexPos(self, indices):
+        return indices[np.random.randint(0, len(indices))]
 
-        def indexToXY(self, index):
-            return (index % self.WIDTH, index // self.WIDTH)
+    def indexToXY(self, index):
+        return (index % self.WIDTH, index // self.WIDTH)
 
-        # === Info Panels ===
-                
-        def display_episode(self,epsiode):
-            font = pygame.font.SysFont(None, 25)
-            text = font.render("Episode: "+str(epsiode), True, (0,0,220))
-            self.DISPLAY.blit(text,(1,1))
+    # === Info Panels ===
+            
+    def display_episode(self,epsiode):
+        font = pygame.font.SysFont(None, 25)
+        text = font.render("Episode: "+str(epsiode), True, (0,0,220))
+        self.DISPLAY.blit(text,(1,1))
 
 
 
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 import numpy as np
 import time
 
+from environment import Env
+
 # Colors
 ORANGE = (255, 165, 0)
 GREEN = (0, 150, 0)
@@ -11,11 +13,11 @@ BLACK = (0,0,0)
 
 display_width, display_height = 800, 900
 
-def show_info(door, hustler, game_display):
+def show_info(goals, busts, game_display):
     pygame.draw.rect(gameDisplay, BLACK, [0, 800, 800, 5])
     font = pygame.font.SysFont(None, 40)
-    text1 = font.render("Total Hustlers Escaped: "+str(door), True, GREEN)
-    text2 = font.render("Total Hustlers Busted: "+str(hustler), True, RED)
+    text1 = font.render("Total Hustlers Escaped: "+str(goals), True, GREEN)
+    text2 = font.render("Total Hustlers Busted: "+str(busts), True, RED)
     
     game_display.blit(text1,(50,810))
     game_display.blit(text2,(50,855))	
@@ -32,13 +34,50 @@ gameDisplay = pygame.display.set_mode((display_width,display_height))
 clock = pygame.time.Clock()
 
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
+# Initialize Environment
+env = Env(gameDisplay, 20, 20) #display, rows, columns
+# set obstacles config if any
+#env.set_obstacles([])
 
-    gameDisplay.fill(WHITE)
+# Initialize Agents
+# cat = Agent(env, possibleActions = 4)
+# mouse = Agent(env, possibleActions = 4)
 
-    pygame.display.update()
-    clock.tick(60)
+# Number of Episodes
+num_episodes = 1000
+
+for i_episode in range(1, num_episodes+1):
+
+    state = env.reset()
+
+    env.render(i_episode)
+
+
+    # Init Action for Agents
+
+    # loop until game is done
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        # env step forward with chosen actions
+        #next_state, reward, done, info = env.step()
+
+        gameDisplay.fill(WHITE) #background color
+        env.render(i_episode)
+        show_info(0, 0, gameDisplay) #goals, busts, game_display
+
+        pygame.display.update()
+        clock.tick(60)
+
+        # if done:
+            # update scores and info
+        #   break
+
+        # update state and next action
+        # if loop:
+        #     action_mouse = np.random.randint(0, 5)
+        #     action_cat = np.random.randint(0, 5)
+        #     loop = False
