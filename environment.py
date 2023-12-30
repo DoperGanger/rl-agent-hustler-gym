a4bc768530d36cdcf8da2673bc78a3c3c9c5494a
0xhatsume <0xhatsume@gmail.com>
2023-12-30 10:48:11 +0800

    :sparkles: Add get state to env obj

--- environment.py
+++ environment.py
@@ -31,7 +31,6 @@ class Env():
             self.OBSTACLES = obstacles
 
         def render(self, i_episode = -1):
-
             # Draw Agents
             self.HUSTLER.draw(self.HUSTLER_X, self.HUSTLER_Y)
             self.CATCHER.draw(self.CATCHER_X, self.CATCHER_Y)
@@ -60,9 +59,9 @@ class Env():
                     indices = np.delete(indices, np.where(indices == v))
 
             # Random set all agent positions
-            self.HUSTLER_X, self.HUSTLER_Y = self.indexToXY(self.getRandom(indices))
-            self.CATCHER_X, self.CATCHER_Y = self.indexToXY(self.getRandom(indices))
-            self.GOAL_X, self.GOAL_Y = self.indexToXY(self.getRandom(indices))
+            self.HUSTLER_X, self.HUSTLER_Y = self.indexToXY(self.getRandomIndexPos(indices))
+            self.CATCHER_X, self.CATCHER_Y = self.indexToXY(self.getRandomIndexPos(indices))
+            self.GOAL_X, self.GOAL_Y = self.indexToXY(self.getRandomIndexPos(indices))
 
             self.MOVES['hustler'] = 100
             self.MOVES['catcher'] = 100
@@ -70,17 +69,74 @@ class Env():
             return self.get_state()
 
         def get_state(self):
-            return
+            '''
+            Return the state for the agent:
+                Hustler:
+                    - Manhattan distance between hustler and catcher
+                    - Manhattan distance between hustler and goal
+                    - Info about walls and obstacles in their neighborood?
+                Catcher:
+                    - Manhattan distance between hustler and catcher
+                    - Info about walls and obstacles in their neighborood?
+            '''
+            self.STATE = {'hustler':(self.HUSTLER_X - self.CATCHER_X, self.HUSTLER_Y - self.CATCHER_Y,
+                                    self.HUSTLER_X - self.GOAL_X, self.HUSTLER_Y -  self.GOAL_Y), 
+                        'catcher':(self.CATCHER_X - self.HUSTLER_X, self.CATCHER_Y - self.HUSTLER_Y)}
 
-        def step(self, huslter_action, catcher_action):
-            return
+            return self.STATE
         
-        def getRandom(self, indices):
+        def step(self, hustler_action, catcher_action):
+            '''
+            Reward update, change angents' position and do some controls about position changes.
+            '''
+            reward = {'hustler':-1, 'catcher':-1}
+            done = False
+            info = {
+                'goal_reached': False,
+                'hustler_caught': False, 
+                'x': -1, 'y': -1, 
+                'width':self.BLOCK_WIDTH, 
+                'height':self.BLOCK_HEIGHT
+            }
+
+            #decreasing the no. of moves
+            self.MOVES['catcher'] -= 1
+            self.MOVES['hustler'] -= 1
+            #done if moves = 0
+            if self.MOVES['catcher'] == 0 or self.MOVES['hustler'] == 0:
+                done = True
+
+            self.update_positions(hustler_action, catcher_action)
+            
+            #hustler reached the goal
+            if self.HUSTLER_X == self.GOAL_X and self.HUSTLER_Y == self.GOAL_Y:
+                done = True
+                reward['hustler'] = 50
+                info['goal_reached'], info['x'], info['y'] = True,  self.HUSTLER_X, self.HUSTLER_Y
+            
+            #catcher caught the hustler
+            if self.CATCHER_X == self.HUSTLER_X and self.CAT_Y == self.HUSTLER_Y:
+                done = True
+                reward['catcher'] = 50
+                reward['hustler'] = -20
+                info['hustler_caught'], info['x'], info['y'] = True,  self.HUSTLER_X, self.HUSTLER_Y
+            
+            for obs in self.OBSTACLES:
+                if self.HUSTLER_X == obs[0] and self.HUSTLER_Y == obs[1]:
+                    reward['hustler'] = -20
+                    self.HUSTLER_X, self.HUSTLER_Y = (0,0)
+
+                if self.CATCHER_X == obs[0] and self.CATCHER_Y == obs[1]:    
+                    reward['catcher'] = -20
+                    self.CATCHER_X, self.CATCHER_Y = (0, self.HEIGHT -1)
+
+            return self.get_state(), reward, done, info
+        
+        def getRandomIndexPos(self, indices):
             return indices[np.random.randint(0, len(indices))]
 
         def indexToXY(self, index):
             return (index % self.WIDTH, index // self.WIDTH)
-
 
         # === Info Panels ===
                 
